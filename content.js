a6290b5869deb1e541cd2a249f2c1612b09eac75
agarwalpooja <poojaag1606@gmail.com>
2020-01-18 18:39:55 +0530

    enabling the popup exdtension for translation enabling

--- content.js
+++ content.js
@@ -1,4 +1,9 @@
-var labels = document.getElementsByTagName('label');
+var flag = 0;
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log("something happening from the extension");
+  var data = request.data || 1;
+    var labels = document.getElementsByTagName('label');
 var output_labels=[]
 
 for (let i = 0, l = labels.length; i < l; i++) {
@@ -7,6 +12,7 @@ for (let i = 0, l = labels.length; i < l; i++) {
 
 // TODOs : Have the original labels stored such that we dont need to hit
 // APIs while toggling between languages.
+var translatedLabels =[]
 
 $.ajax({
   url: "https://hackapi.reverieinc.com/nmt",
@@ -21,13 +27,33 @@ $.ajax({
     "tgt" : "hi",
     "src" : "en"
   }),
-  success: function(res){
+  success: function (res) {
+    flag = 1;
     console.log(res.data.result.flat(1));
-    var translatedLabels = res.data.result.flat(1);
+    translatedLabels = res.data.result.flat(1);
     for (let i = 0, l = labels.length; i < l; i++) {
       labels[i].innerText = translatedLabels[i];
     }
     console.log(labels);
+    $.ajax({
+      url: "https://hackapi.reverieinc.com/tts",
+      headers: { 
+        'Content-Type':'application/json',
+        'token': '106c1c674bd9b81ece4194667167f68ba46fbe03'
+      },
+      method: 'POST',
+      dataType: 'json',
+      "data": JSON.stringify({
+        "text" : translatedLabels[0],
+        "lang" : "hi",
+      }),
+      success: function(res){
+        filename = test.wav;
+        
+        
+      }    
+    });
   }
 });
-
+  sendResponse({data: data, success: true});
+});
